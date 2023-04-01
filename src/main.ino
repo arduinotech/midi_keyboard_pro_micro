7aaddff9b08e9876388493186c226a4e04384b50
Dmitriy Beketov <beketov@calltouch.net>
2023-04-01 11:51:45 +0300

    It work!

--- src/main.ino
+++ src/main.ino
@@ -1,53 +1,88 @@
 #include <Control_Surface.h> // Include the Control Surface library
+#include <Arduino.h>
+// #include <Streaming.h>
 
-// Instantiate a MIDI over USB interface.
 USBMIDI_Interface midi;
 
 using namespace MIDI_Notes;
 
-// Instantiate a NoteButton object
-NoteButton button2 = {
-  2,                       // Push button on pin 5
-  {note(C, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
+int rowPinMap[8] = {6, 7, 8, 9, 10, 16, 14, 15};
+int colPinMap[4] = {2, 3, 4, 5};
+
+bool buttonsState[8][4] = {
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false},
+    {false, false, false, false}
 };
-NoteButton button3 = {
-  3,                       // Push button on pin 5
-  {note(D, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
+
+int keysNum[8][4] = {
+    {7, 15, 23, 31},
+    {6, 14, 22, 30},
+    {5, 13, 21, 29},
+    {4, 12, 20, 28},
+    {0, 8, 16, 24},
+    {1, 9, 17, 25},
+    {2, 10, 18, 26},
+    {3, 11, 19, 27}
 };
-NoteButton button4 = {
-  4,                       // Push button on pin 5
-  {note(E, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
+
+MIDIAddress keysAddress[32] = {
+    {note(F, 2), CHANNEL_1}, {note(Gb, 2), CHANNEL_1}, {note(G, 2), CHANNEL_1}, {note(Ab, 2), CHANNEL_1},
+    {note(A, 2), CHANNEL_1}, {note(Bb, 2), CHANNEL_1}, {note(B, 2), CHANNEL_1}, {note(C, 3), CHANNEL_1},
+    {note(Db, 3), CHANNEL_1}, {note(D, 3), CHANNEL_1}, {note(Eb, 3), CHANNEL_1}, {note(E, 3), CHANNEL_1},
+    {note(F, 3), CHANNEL_1}, {note(Gb, 3), CHANNEL_1}, {note(G, 3), CHANNEL_1}, {note(Ab, 3), CHANNEL_1},
+    {note(A, 3), CHANNEL_1}, {note(Bb, 3), CHANNEL_1}, {note(B, 3), CHANNEL_1}, {note(C, 4), CHANNEL_1},
+    {note(Db, 4), CHANNEL_1}, {note(D, 4), CHANNEL_1}, {note(Eb, 4), CHANNEL_1}, {note(E, 4), CHANNEL_1},
+    {note(F, 4), CHANNEL_1}, {note(Gb, 4), CHANNEL_1}, {note(G, 4), CHANNEL_1}, {note(Ab, 4), CHANNEL_1},
+    {note(A, 4), CHANNEL_1}, {note(Bb, 4), CHANNEL_1}, {note(B, 4), CHANNEL_1}, {note(C, 5), CHANNEL_1}
 };
-NoteButton button5 = {
-  5,                       // Push button on pin 5
-  {note(F, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
-};
-NoteButton button6 = {
-  6,                       // Push button on pin 5
-  {note(G, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
-};
-NoteButton button7 = {
-  7,                       // Push button on pin 5
-  {note(A, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
-};
-NoteButton button8 = {
-  8,                       // Push button on pin 5
-  {note(B, 4), CHANNEL_1}, // Note C4 on MIDI channel 1
-};
+
+const uint8_t velocity = 0x7F;
 
 void setup()
 {
-    button2.invert();
-    button3.invert();
-    button4.invert();
-    button5.invert();
-    button6.invert();
-    button7.invert();
-    button8.invert();
-    Control_Surface.begin(); // Initialize Control Surface
+    // Serial.begin(115200);
+
+    for (int row = 0; row < 8; row++)
+        pinMode(rowPinMap[row], INPUT_PULLUP);
+
+    for (int col = 0; col < 4; col++)
+        pinMode(colPinMap[col], INPUT_PULLUP);
+
+    Control_Surface.begin();
 }
 
 void loop()
 {
     Control_Surface.loop(); // Update the Control Surface
+
+    for (int row = 0; row < 8; row++) {
+        int rowPin = rowPinMap[row];
+        pinMode(rowPin, OUTPUT);
+        digitalWrite(rowPin, LOW);
+        for (int col = 0; col < 4; col++) {
+            bool isKeyDown = !digitalRead(colPinMap[col]);
+            if (isKeyDown) {
+                if (!buttonsState[row][col]) {
+                    buttonsState[row][col] = true;
+                    midi.sendNoteOn(keysAddress[keysNum[row][col]], velocity);
+                    // Serial << "button[" << row << "][" << col << "] - " << keysNum[row][col] << " - DOWN\n";
+                }
+            } else {
+                if (buttonsState[row][col]) {
+                    buttonsState[row][col] = false;
+                    midi.sendNoteOff(keysAddress[keysNum[row][col]], velocity);
+                    // Serial << "button[" << row << "][" << col << "] - " << keysNum[row][col] << " - UP\n";
+                }
+            }
+        }
+        digitalWrite(rowPin, HIGH);
+        // delay(1);
+        pinMode(rowPin, INPUT_PULLUP);
+    }
 }
